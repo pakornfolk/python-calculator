9be764f45e4ec3321b64d3f515bdf0461f4e76d9
pakornfolk <pakornkiat.von@student.mahidol.edu>
2025-11-13 11:30:57 +0700

    Added the test cases

--- test_calculator.py
+++ test_calculator.py
@@ -7,16 +7,16 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(6, 9), 3)
 
     # Add the following test methods to the TestCalculator class:
 
 # add() --- (2 test cases)
     def test_add_positive(self):
-        self.assertEqual(self.calc.add(3, 7), 10)
+        self.assertEqual(self.calc.add(5, 7), 12)
 
     def test_add_negative(self):
-        self.assertEqual(self.calc.add(-5, 2), -3)
+        self.assertEqual(self.calc.add(-5, 3), -2)
 
 # subtract() --- (2 test cases)
     def test_subtract_basic(self):
@@ -27,24 +27,24 @@ class TestCalculator(unittest.TestCase):
 
 # multiply() --- (2 test cases)
     def test_multiply_normal(self):
-        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(7, 4), 28)
 
     def test_multiply_zero(self):
         self.assertEqual(self.calc.multiply(7, 0), 0)
 
 # divide() --- (2 test cases)
     def test_divide_basic(self):
-        self.assertEqual(self.calc.divide(8, 2), 4)
+        self.assertEqual(self.calc.divide(6, 3), 2)
 
     def test_divide_equal_case(self):
-        self.assertEqual(self.calc.divide(4, 4), 1)
+        self.assertEqual(self.calc.divide(8, 2), 4)
 
 # modulo() --- (2 test cases)
     def test_modulo_basic(self):
-        self.assertEqual(self.calc.modulo(10, 3), 1)
+        self.assertEqual(self.calc.modulo(17, 5), 2)
 
     def test_modulo_equal_case(self):
-        self.assertEqual(self.calc.modulo(6, 3), 0)
+        self.assertEqual(self.calc.modulo(9, 3), 0)
 
 
 if __name__ == '__main__':
